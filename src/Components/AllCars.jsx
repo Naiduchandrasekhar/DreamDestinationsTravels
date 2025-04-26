6f0b8523ea47012dd1a81b79aeba4f980adbf071
Naiduchandrasekhar <naiduchandrasekhar10@gmail.com>
2025-04-26 01:35:44 -0700

    filters added in mobile screen

--- src/Components/AllCars.jsx
+++ src/Components/AllCars.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import carFilterIcon from "../Images/carFilterIcon.png";
 import carwalDzireImage from "../Images/carwaleDzireImage.jpg";
 import urbaniaVanImage from "../Images/urbaniaVanImage.jpg";
 import grandVitaraCarImage from "../Images/grandVitaraCarImage.webp";
@@ -18,7 +19,7 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
             brand: "Maruti Suzuki",
             model: "Dzire",
             image: carwalDzireImage,
-            price: "₹ 3000",
+            price: "3000",
             seats: "4"
         },
 
@@ -27,7 +28,7 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
             brand: "Maruti Suzuki",
             model: "Grand Vitara",
             image: grandVitaraCarImage,
-            price: "₹ 3500",
+            price: "3500",
             seats: "6"
         },
         {
@@ -35,7 +36,7 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
             brand: "Maruti Suzuki",
             model: "Ertiga",
             image: ertigaCarWaleImage,
-            price: "₹ 4000",
+            price: "4000",
             seats: "6"
         },
         {
@@ -43,7 +44,7 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
             brand: "Maruti Suzuki",
             model: "Swift VDI",
             image: swiftVDICarwaleImage,
-            price: "₹ 3000",
+            price: "3000",
             seats: "4"
         },
         {
@@ -51,7 +52,7 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
             brand: "Toyota",
             model: "Crysta",
             image: innovaCrystaCarwaleImage,
-            price: "₹ 4500",
+            price: "4500",
             seats: "7"
         },
         {
@@ -67,7 +68,7 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
             brand: "Force Motors",
             model: "Urbania",
             image: urbaniaVanImage,
-            price: "₹ 7000",
+            price: "7000",
             seats: "17"
         },
         {
@@ -75,7 +76,7 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
             brand: "Mahindra",
             model: "XUV 700",
             image: xuvCarImage,
-            price: "₹ 4500",
+            price: "4500",
             seats: "6"
         }
     ];
@@ -150,18 +151,64 @@ const AllCars = ({ hideViewMoreCarsBtn, hideFilterSection }) => {
                 (!filterData.price || filterData.price.length === 0 || filterData.price.includes(String(car.price)))
             );
         });
-    
+
         setFilteredCars(filtered);
     }, [filterData]);
-    
+
 
 
     return (
         <div>
             <div className='container'>
                 <div className='py-3'>
-                    <div className='d-flex align-items-start'>
-                        <div className={`${hideFilterSection ? "col-lg-12" : "col-lg-9"} d-flex justify-content-center justify-content-lg-between flex-wrap`}>
+                    <div className='d-flex d-lg-none justify-content-center align-items-center mb-1' data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">
+                        <button className="btn filterButtonText" type="button" >Filter your favourite car</button>
+                        <div className='mx-2'>
+                            <img className='carFilterIcon' src={carFilterIcon} alt='carFilterIcon' />
+                        </div>
+                    </div>
+                    <div className="offcanvas offcanvas-bottom" tabIndex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+                        <div className="offcanvas-header">
+                            <h5>Filter Cars</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body small">
+                            <div className="d-flex d-lg-none d-flex flex-column justify-content-center filterCarSectionContainer">
+                                {/* Filter Section Template */}
+                                {[
+                                    { label: "Brand", options: ["Maruti Suzuki", "Toyota", "Force Motors", "Mahindra"], name: "brand" },
+                                    { label: "Model", options: ["Dzire", "Grand Vitara", "Ertiga", "Swift VDI", "Crysta", "Hycross", "Urbania", "XUV 700"], name: "model" },
+                                    { label: "Seats", options: ["4", "6", "7", "17"], name: "seats", format: val => `${val} Seats` },
+                                    { label: "Price", options: ["3000", "3500", "4000", "4500", "7000"], name: "price", format: val => `₹ ${val}` }
+                                ].map((filter, idx) => (
+                                    <div className="mb-3" key={idx}>
+                                        <strong>{filter.label}</strong>
+                                        <div className="d-flex flex-wrap gap-2 fontSize14 mt-2">
+                                            {filter.options.map((option, i) => (
+                                                <div className="form-check" key={i} style={{ minWidth: '48%' }}>
+                                                    <input
+                                                        id={`${filter.name}-${option}`}
+                                                        className="form-check-input cursorPointer"
+                                                        type="checkbox"
+                                                        name={filter.name}
+                                                        value={option}
+                                                        onChange={handleCheckboxChange}
+                                                        checked={filterData[filter?.name]?.includes(option)}
+                                                    />
+                                                    <label htmlFor={`${filter.name}-${option}`} className="form-check-label cursorPointer">
+                                                        {filter.format ? filter.format(option) : option}
+                                                    </label>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                ))}
+                                <button className="btn btn-secondary w-100" onClick={handleClearAllFilters}>Clear All</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='d-flex justify-content-center justify-content-lg-start align-items-start'>
+                        <div className={`${hideFilterSection ? "col-lg-12" : "col-lg-9"} d-flex justify-content-center justify-content-lg-start flex-wrap`}>
                             {
                                 filteredCars?.map((carDetails) => {
                                     return (
